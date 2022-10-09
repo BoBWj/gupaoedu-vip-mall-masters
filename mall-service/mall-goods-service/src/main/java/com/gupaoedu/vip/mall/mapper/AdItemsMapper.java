5a0f7ec06aa5c459d693028b2074e5be3d1bf2f3
sunshineinthedark <945534585@qq.com>
2022-10-09 21:48:28 +0800

    1

---
+++ mall-service/mall-goods-service/src/main/java/com/gupaoedu/vip/mall/mapper/AdItemsMapper.java
@@ -0,0 +1,10 @@
+package com.gupaoedu.vip.mall.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gupaoedu.mall.goods.model.ADItems;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AdItemsMapper extends BaseMapper<ADItems > {
+}
